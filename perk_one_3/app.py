758c1aca2b093745184d11e68d7edef5daa6b12f
Tony <Tony_zherka@hotmail.co.uk>
2022-05-04 12:24:34 +0000

    Tz: got working perk1 Generator

--- perk_one_3/app.py
+++ perk_one_3/app.py
@@ -1,6 +1,16 @@
 from flask import Flask, jsonify
 import random
+
 app = Flask(__name__)
+
+perk_one = {'Faster Reload', 'Increased FireRate', 'Enhanced Movement', 'Infinity', 'Increased Bullet Damage',
+'Increased Melee Damage','Increased Crit Chance', 'Increased Crit Damage', 'Increased Accuracy', 'Increased Range',}
+
+@app.route('/get_perk1')
+def get_perk1():
+    return jsonify(random.choice(list(perk_one)))
+   
+    
 
 
 if __name__=='__main__': app.run(host = "0.0.0.0", port=5000, debug=True)
