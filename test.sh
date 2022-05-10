9dff1edbff42b713419f306af75701bd1aec6d2a
Tony <Tony_zherka@hotmail.co.uk>
2022-05-10 15:11:27 +0000

    "Tz: updated files"

--- test.sh
+++ test.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-# python3 -m venv venv
-
 # sudo apt install python3.8-venv
 
 # source venv/bin/activate
@@ -9,6 +7,7 @@
 # pip3 install -r requirements.txt
 
 # python3 -m pytest --cov=.
+python3 -m venv venv
 ls -l
 source venv/bin/activate
 declare -a directories=("front_end_1" "gun_gen_2" "perk_one_3" "perk_two_4")
